c12ec29a01b83a8b8e1c547c77549400425b0aa7
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-06-30 19:57:53 +0200

    Added videos to the assets directory

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -20,12 +20,21 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class ProjectsComponent implements OnInit {
   flip: string = 'inactive';
-  flip1: string = 'active';
-  flip2: string = 'active';
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  flip1: string = 'inactive';
+  flip2: string = 'inactive';
+  flip3: string = 'inactive';
+
+  longText = `This shows my first project detailing basic functionality of React.js. Basic stying
+   and developing components. Here, I only used class based componets`;
+  longText1 = `I explored some of the major functionality of React.js. This explored interraction
+  of events and data. React router and click events, change events that interracts with data and
+  display users request with clicks. Some functionality is not implemented `;
+  longText2 = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
-
+  longText3 = `API, Database, useEffects, useState some of the functionality used in Modern website was used here.
+  I also explored the use of styled components, react-router-dom and redux. `;
+  
   constructor() { }
 
   ngOnInit(): void{ };
@@ -33,12 +42,13 @@ export class ProjectsComponent implements OnInit {
   toggleFlip() {
     this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
   }
-
   toggleFlip1() {
-    this.flip1 = (this.flip1 == 'active') ? 'inactive' : 'active';
+    this.flip1 = (this.flip1 == 'inactive') ? 'active' : 'inactive';
   }
-
   toggleFlip2() {
-    this.flip2 = (this.flip2 == 'active') ? 'inactive' : 'active';
+    this.flip2 = (this.flip2 == 'inactive') ? 'active' : 'inactive';
+  }
+   toggleFlip3() {
+    this.flip3 = (this.flip3 == 'inactive') ? 'active' : 'inactive';
   }
 }
