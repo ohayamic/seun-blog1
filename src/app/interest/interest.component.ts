0769e9b61f80676649396ce0541a07351bdf8def
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-07-05 22:12:09 +0200

    Started building the interest component

--- src/app/interest/interest.component.ts
+++ src/app/interest/interest.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interest.component.css']
 })
 export class InterestComponent implements OnInit {
+ interests: any = ["undraw_javascript_frameworks_-8-qpc.svg", "undraw_researching_re_fuod.svg",
+                  "undraw_cloud_hosting_re_mkg7.svg", "undraw_working_remotely_re_6b3a.svg",
+                  "undraw_personal_trainer_re_cnua.svg", "undraw_sharing_knowledge_03vp.svg"]
+
+ skills: any = ["undraw_home_screen_re_640d.svg", "undraw_artificial_intelligence_re_enpp.svg",
+                "undraw_work_together_re_5yhn.svg", "undraw_time_management_re_tk5w.svg",
+               ]
 
   constructor() { }
 
