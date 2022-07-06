1957df8c7175e352d077f9bdf89354b20d48c159
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-07-06 22:47:27 +0200

    installed and created express server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+app.use(express.static('./dist/seun-blog1'));
 
-app.listen(process.env.PORT || 4200);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/seun-blog1/'}),
+);
+
+app.listen(process.env.PORT || 8080);
