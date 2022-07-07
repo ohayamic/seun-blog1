54b8fe6eea4cc5c572fba1430b64d8160f2a9cd6
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-07-07 23:39:38 +0200

    Finished the internship story

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -20,12 +20,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class LandingPageComponent implements OnInit {
 flip: string = 'inactive';
-  flip1: string = 'active';
+  flip1: string = 'inactive';
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
     from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
     originally bred for hunting.`;
   FirstInternship = ` My first internship was very exciting, alone in a foreign land with temperature below zero and a very huge cultural difference,
-  I was welcomed by my collegues with open arms. This helped me to manage the shock considerably. My `
+  I was welcomed by my collegues with open arms. This helped me to manage the shock considerably. The first day was for onboarding and before long it
+  was as if I had beeen with the firm for a long time. I was involved in mini-projects, programming conferences and even handle company meetings.  `
+  Internship_skills = ['', '', '', '', '', '']
+
   Projects = `I have always been facinated learning new technologies so when the opportunity came for me to write my thesis in ML, I jumped
    at it. Being a very curious person, I painstaking learnt the fundamental concepts of the technology. This gave me a solid understanding to grasp
    complex topics down the line. In all, though it was challenging it was all worth it and I continue to leverage the knowledge to this day.`
@@ -40,6 +43,6 @@ flip: string = 'inactive';
   }
 
   toggleFlip1() {
-    this.flip1 = (this.flip1 == 'active') ? 'inactive' : 'active';
+    this.flip1 = (this.flip1 == 'inactive') ? 'active' : 'inactive';
   }
 }
