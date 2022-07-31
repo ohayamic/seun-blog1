4471d3384af8b9306171271bb98b0d02a5e6ea8d
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-07-31 22:24:13 +0200

    Added design component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { ProfileComponent} from './profile/profile.component';
 import { InterestComponent } from './interest/interest.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DesignComponent } from './design/design.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'interest', component: InterestComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'design', component: DesignComponent },
+
 ];
 
 @NgModule({
